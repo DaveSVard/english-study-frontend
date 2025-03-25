b99216bd860eada1ab2e16802e3acaec0f39e313
DaveSVard <vardanyan003@gmail.com>
2025-03-25 23:36:13 +0400

    Created Animated Testimonials carousel section

--- src/app/lib/hooks.tsx
+++ src/app/lib/hooks.tsx
@@ -26,3 +26,14 @@ export function useWindowSize() {
 
   return windowSize;
 }
+
+// Custom hook for checking if the component is mounted
+export const useMounted = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return mounted;
+};
